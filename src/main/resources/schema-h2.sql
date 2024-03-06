6c03d095d9b52b478cf8698272d4d39125f04159
Johan Gangsås Hole <johan@devotta.no>
2024-03-06 12:24:20 +0100

    feat: dead letter channel and success log for event delivery

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -14,6 +14,18 @@ CREATE TABLE IF NOT EXISTS REPORT_DEAD_LETTER_CHANNEL (
 
 CREATE INDEX ON REPORT_DEAD_LETTER_CHANNEL(status);
 
+CREATE TABLE IF NOT EXISTS EVENT_DEAD_LETTER_CHANNEL (
+    id                      INTEGER                             PRIMARY KEY AUTO_INCREMENT,
+    payload                 VARCHAR                             NOT NULL,
+    event_id                VARCHAR                             NOT NULL,
+    error_message           VARCHAR                             NOT NULL,
+    status                  ENUM('PROCESSED', 'RETRY', 'ERROR') NOT NULL,
+    created_at              TIMESTAMP WITH TIME ZONE            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_processed_at       TIMESTAMP WITH TIME ZONE            NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX ON EVENT_DEAD_LETTER_CHANNEL(status);
+
 CREATE TABLE IF NOT EXISTS POLLER (
     flow_uuid    VARCHAR                   PRIMARY KEY,
     last_run_at  TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -23,7 +35,6 @@ CREATE TABLE IF NOT EXISTS TOKEN (
     value_  VARCHAR PRIMARY KEY
 );
 
--- TABLE MIGRATION - to be deprecated
 ALTER TABLE IF EXISTS SUCCESS_LOG RENAME TO REPORT_SUCCESS_LOG;
 
 CREATE TABLE IF NOT EXISTS REPORT_SUCCESS_LOG (
@@ -34,5 +45,14 @@ CREATE TABLE IF NOT EXISTS REPORT_SUCCESS_LOG (
     data_set_code           VARCHAR                             NOT NULL,
     report_period_offset    INTEGER                             NOT NULL,
     organisation_unit_id    VARCHAR,
+    created_at              TIMESTAMP WITH TIME ZONE            NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS EVENT_SUCCESS_LOG (
+    id                      INTEGER                             PRIMARY KEY AUTO_INCREMENT,
+    dhis_request            VARCHAR                             NOT NULL,
+    dhis_response           VARCHAR                             NOT NULL,
+    rapidpro_payload        VARCHAR                             NOT NULL,
+    event_id                VARCHAR                             NOT NULL,
     created_at              TIMESTAMP WITH TIME ZONE            NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -8,6 +8,7 @@ BEGIN
     END IF;
 END
 $$;
+
 CREATE TABLE IF NOT EXISTS REPORT_DEAD_LETTER_CHANNEL (
     id                      BIGSERIAL                   PRIMARY KEY,
     payload                 VARCHAR                     NOT NULL,
@@ -21,6 +22,18 @@ CREATE TABLE IF NOT EXISTS REPORT_DEAD_LETTER_CHANNEL (
 );
 
 CREATE INDEX ON REPORT_DEAD_LETTER_CHANNEL(status);
+
+CREATE TABLE IF NOT EXISTS EVENT_DEAD_LETTER_CHANNEL (
+    id                      BIGSERIAL                   PRIMARY KEY,
+    payload                 VARCHAR                     NOT NULL,
+    event_id                VARCHAR                     NOT NULL,
+    error_message           VARCHAR                     NOT NULL,
+    status                  VARCHAR                     NOT NULL CHECK (status = 'PROCESSED' OR status = 'RETRY' OR status = 'ERROR'),
+    created_at              TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_processed_at       TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX ON EVENT_DEAD_LETTER_CHANNEL(status);
 
 CREATE TABLE IF NOT EXISTS POLLER (
     flow_uuid    VARCHAR                   PRIMARY KEY,
@@ -39,5 +52,14 @@ CREATE TABLE IF NOT EXISTS REPORT_SUCCESS_LOG (
     data_set_code           VARCHAR                             NOT NULL,
     report_period_offset    INTEGER                             NOT NULL,
     organisation_unit_id    VARCHAR,
+    created_at              TIMESTAMP WITH TIME ZONE            NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS EVENT_SUCCESS_LOG (
+    id                      BIGSERIAL                           PRIMARY KEY,
+    dhis_request            VARCHAR                             NOT NULL,
+    dhis_response           VARCHAR                             NOT NULL,
+    rapidpro_payload        VARCHAR                             NOT NULL,
+    event_id                VARCHAR                             NOT NULL,
     created_at              TIMESTAMP WITH TIME ZONE            NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
